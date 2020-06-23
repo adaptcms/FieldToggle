116f5908e71377e8c1f3e4963e87e86e2dff17c8
Charlie Page <charliepage88@gmail.com>
2020-06-23 19:12:12 -0400

    sync

--- src/FieldToggle.php
+++ src/FieldToggle.php
@@ -2,7 +2,17 @@
 
 namespace Adaptcms\FieldToggle;
 
+use Adaptcms\Base\Models\Package;
+
 class FieldToggle
 {
-    // Build wonderful things
+  /**
+  * On Install
+  *
+  * @return void
+  */
+  public function onInstall()
+  {
+    Package::syncPackageFolder(get_class());
+  }
 }
